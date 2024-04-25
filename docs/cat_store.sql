197c11b5ec08afe346ec1a4e92242b9fdaa6bea9
Pham Thi Quynh Anh <quynhanhphamthi01@gmail.com>
2024-04-25 22:55:21 +0700

    add sql file + model sql

--- docs/cat_store.sql
+++ docs/cat_store.sql
@@ -1,36 +1,38 @@
 create database cat_store;
 use cat_store;
 
+-- drop database cat_store;
+
 create table role(
     id int primary key AUTO_INCREMENT,
     roleName nvarchar(255)
 );
 
-insert into role values ('', 'admin'),
-                        ('', 'customer');
+INSERT INTO `cat_store`.`role` (`roleName`) VALUES ('admin');
+INSERT INTO `cat_store`.`role` (`roleName`) VALUES ('user');
 
 create table user(
     id int primary key AUTO_INCREMENT,
     username varchar(255),
     password varchar(255),
-    address nvarchar(255),
+    address varchar(255),
     phone varchar(10),
     email varchar(255),
-    roleId int,
+    roleId int DEFAULT 2,
+    create_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    update_at datetime,
     foreign key (roleId) references role(id)
 );
 
-insert into user values ('', 'anh', '123', '1')
-                        ('', 'cus', '123', '2');
+INSERT INTO `cat_store`.`user` (`username`, `password`) VALUES ('huong', '2');
+INSERT INTO `cat_store`.`user` (`username`, `password`, `roleId`) VALUES ('anh', '1', '1');
 
 create table review(
     id int primary key AUTO_INCREMENT,
-    content nvarchar(255),
+    content varchar(255),
     rating int,
     customerId int,
     foreign key (customerId) references user(id)
 );
-
-insert into values('', 'good', '5', '1');
 
 
